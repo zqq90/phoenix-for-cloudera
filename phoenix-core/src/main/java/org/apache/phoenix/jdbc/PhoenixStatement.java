75caf3d5d52c13116fdf1ea8a5759cecc578ad7a
Andrew Purtell <apurtell@salesforce.com>
2015-09-15 16:03:35 -0700

    Remove use of TraceQueryPlan (removed by CDH) from PhoenixStatement
    (Cherry picked from 0f86db2e2dfdc7fe8f0c4ddafff29ffac9448442)

--- phoenix-core/src/main/java/org/apache/phoenix/jdbc/PhoenixStatement.java
+++ phoenix-core/src/main/java/org/apache/phoenix/jdbc/PhoenixStatement.java
@@ -68,7 +68,6 @@ import org.apache.phoenix.compile.StatementNormalizer;
 import org.apache.phoenix.compile.StatementPlan;
 import org.apache.phoenix.compile.SubqueryRewriter;
 import org.apache.phoenix.compile.SubselectRewriter;
-import org.apache.phoenix.compile.TraceQueryPlan;
 import org.apache.phoenix.compile.UpsertCompiler;
 import org.apache.phoenix.coprocessor.MetaDataProtocol;
 import org.apache.phoenix.exception.BatchUpdateExecution;
@@ -974,7 +973,7 @@ public class PhoenixStatement implements Statement, SQLCloseable, org.apache.pho
         @SuppressWarnings("unchecked")
         @Override
         public QueryPlan compilePlan(final PhoenixStatement stmt, Sequence.ValueOp seqAction) throws SQLException {
-            return new TraceQueryPlan(this, stmt);
+            throw new UnsupportedOperationException("Tracing is not supported");
         }
     }
 
